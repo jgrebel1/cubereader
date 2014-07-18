90918d9462a5c06494b7a194cf2ff6ad2a80aef4
Joel G <joel.g.07@gmail.com>
2014-07-18 15:04:19 -0700

    added load_fit and load_data

--- file_tools.py
+++ file_tools.py
@@ -1,11 +1,17 @@
 import sys
 from PySide import QtGui
 
-def file_dialog():
+#project specific items
+import data_holder
+import analysis
+import data_view
+
+def file_dialog(title=None):
     app = QtGui.QApplication.instance()
     if app is None:
         app = QtGui.QApplication(sys.argv)
     dialog = QtGui.QFileDialog()
+    dialog.setWindowTitle(title)
     dialog.setFileMode(QtGui.QFileDialog.ExistingFile)
     dialog.setNameFilter('HDF5 (*.hdf5)')
     if dialog.exec_():
@@ -16,3 +22,15 @@ def file_dialog():
         else:
             print 'No file selected'
     return filename
+
+def load_data(filename):
+    data = data_holder.Data(filename)
+    maxval = analysis.find_maxval(data.ycube[...])
+    dimensions = analysis.get_dimensions(data.ycube)
+    dataview = data_view.DataView(maxval, dimensions)
+    return data, dataview
+
+def load_fit(filename):
+    fit_data = data_holder.FitData(filename)           
+    fit_data_view = data_view.FitDataView()  
+    return fit_data, fit_data_view

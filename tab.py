ca4b679084cf056a70b89fdecfc3a9613ea173bc
Joel G <joel.g.07@gmail.com>
2014-07-22 14:20:36 -0700

    changed to use view_windows

--- tab.py
+++ tab.py
@@ -39,6 +39,8 @@ import spectrum_holder
 import data_holder
 import convert_to_ev
 import navigation_tools
+import h_cubereader
+import view_windows
 
 plt.ioff()
             
@@ -48,11 +50,10 @@ class Tab(QtGui.QWidget):
         super(Tab, self).__init__(parent)      
         self.filename = filename
         print self.filename
+        self.cube = h_cubereader.load_data(filename)
         self.basename = analysis.get_file_basename(self.filename)
-        self.data = data_holder.Data(self.filename)
-        self.maxval = analysis.find_maxval(self.data.ycube[...])
-        dimensions = analysis.get_dimensions(self.data.ycube) 
-        self.dataview = data_view.DataView(self.maxval, dimensions)
+        self.data = self.cube[0]
+        self.dataview = self.cube[1]
         self.convert_mutex = QtCore.QMutex()
         self.bool_press = False
         self.make_spectrum_holder()
@@ -61,33 +62,10 @@ class Tab(QtGui.QWidget):
     
     def make_frame(self):
         """populate screen"""
-
-        self.fig = plt.figure(figsize=(16.0, 6.0))
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self)            
-        self.img_axes = self.fig.add_subplot(121)
-        self.img = plot_tools.initialize_image(self.img_axes,
-                                               self.data,
-                                               self.dataview)
-        self.marker, = self.img_axes.plot(0,0,'wo')
-        self.cbar = plt.colorbar(self.img)
-        self.set_color_bar_settings()
-        self.graph_axes = self.fig.add_subplot(122)  
-        self.img2 = plot_tools.initialize_graph(self.graph_axes,
-                                                self.data,
-                                                self.dataview)
-
-        # Create the navigation toolbar, tied to the canvas
         #
-        self.mpl_toolbar = NavigationToolbar(self.canvas, self)
-        #
-        # Layout with box sizers
-        left_spacer = QtGui.QWidget()
-        left_spacer.setSizePolicy(QtGui.QSizePolicy.Expanding,
-                                  QtGui.QSizePolicy.Expanding)      
-        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
-        self.set_slider_settings()
-        
+        # Layout 
+        self.view = view_windows.data(self.cube)
+
         self.button_display_header = QtGui.QPushButton('Display Header')
         self.button_display_header.setSizePolicy(QtGui.QSizePolicy.Fixed, 
                                                QtGui.QSizePolicy.Fixed)
@@ -134,13 +112,8 @@ class Tab(QtGui.QWidget):
         self.initialize_vbox(self.label_vmin_slice, self.label_vmax_slice,
                                self.textbox_vmin_slice, self.textbox_vmax_slice)
         
-        hbox = QtGui.QHBoxLayout()
         vbox = QtGui.QVBoxLayout()
-        vbox.addWidget(self.mpl_toolbar)       
-        vbox.addWidget(self.canvas)
-        hbox.addWidget(left_spacer)
-        hbox.addWidget(self.slider)
-        vbox.addLayout(hbox)
+        vbox.addWidget(self.view)
         vbox.addWidget(self.button_display_header)
         vbox.addWidget(self.button_export_spectrum)
         vbox.addWidget(self.button_export_cube)
@@ -158,9 +131,6 @@ class Tab(QtGui.QWidget):
 
         self.setLayout(vbox)
         
-        self.connect_events()
-        self.connect_shortcuts()
-
 
     def change_display(self):
         """
@@ -240,12 +210,8 @@ class Tab(QtGui.QWidget):
     
     def initialize_vbox(self,label_vmin_slice, label_vmax_slice,
                              textbox_vmin_slice, textbox_vmax_slice):
-        if self.dataview.display_ev:
-            self.label_vmin_slice.setText('Min ev:')
-            self.label_vmax_slice.setText('Max ev:')
-        else:
-            self.label_vmin_slice.setText('Min wavelength:')
-            self.label_vmax_slice.setText('Max wavelength:')
+        self.label_vmin_slice.setText('Min')
+        self.label_vmax_slice.setText('Max')
         xdata = analysis.xdata_calc(self.data, self.dataview)
         textbox_vmin_slice.setText(str(xdata[0]))
         textbox_vmax_slice.setText(str(xdata[-1]))
@@ -264,72 +230,6 @@ class Tab(QtGui.QWidget):
         self.spectrum_holder = spectrum_holder.SpectrumHolder(self.filename,
                                                               self.dataview.dimension1,
                                                               self.dataview.dimension2)
-                                                              
-    def move_down(self):
-        """move marker down and update graph"""
-        navigation_tools.move_down(self.dataview, self.dataview.dimension1)
-        self.update_graph()
-        
-    
-    def move_left(self):
-        """move marker left and update graph"""
-        navigation_tools.move_left(self.dataview)
-        self.update_graph()
-    
-    def move_right(self):
-        """move marker right and update graph"""
-        navigation_tools.move_right(self.dataview, self.dataview.dimension2)
-        self.update_graph()
-      
-    def move_up(self):
-        """move marker up and update graph"""
-        navigation_tools.move_up(self.dataview)
-        self.update_graph()
-      
-    def on_motion(self, event):        
-        """
-        dragging the marker will change the graph.
-        """
-        if self.bool_press is False: return
-        if event.inaxes != self.img.axes: return
-    
-        navigation_tools.change_coordinates(event, self.dataview)
-        self.update_graph()
-        self.canvas.draw()
-
-        
-    def on_pick_color(self, event):
-        """
-        Clicking on the color bar will generate three different actions 
-        depending on location. The upper third sets the max color value,
-        the lower third sets min color value, and the middle pops up a
-        window asking for custom values.
-        """
-        color.on_pick_color_cube(event, self.img, self.data, self.dataview)
-        self.canvas.draw()      
-        
-    def on_press_image(self, event):
-        """
-        This changes the spec graph to theclicked xy coordinate and moves
-        the marker on the image.
-        
-        The .5 addition in the floor function is there to make xdata print 
-        correct coordinates.        
-        """
-        if event.inaxes != self.img.axes: return    
-        contains, attrd = self.img.contains(event)
-    
-        if not contains: return
-            
-        navigation_tools.change_coordinates(event, self.dataview)
-        self.bool_press = True
-        self.update_graph()
-
-
-    def on_release(self, event):
-        'on release we reset the press data'
-        self.bool_press = False
-    
     def open_visualization(self):
         """opens mayavi window"""
         self.data.check_for_ev_cube(self.data.hdf5)
@@ -351,57 +251,8 @@ class Tab(QtGui.QWidget):
         self.wraith_window = wraith_for_cubereader.Form(self.data,
                                                         self.dataview,
                                                         self.spectrum_holder)
-        self.wraith_window.show()                                                  
-       
-    def reset_colors(self):
-        color.reset_colors_cube(self.img, self.data, self.dataview)           
-                
-    def set_color_bar_settings(self):
-        self.dataview.maxcolor = self.maxval
-        self.dataview.mincolor = 0
-        self.cbar.ax.set_picker(5) 
-        
-    def set_slider_settings(self):
-        self.slider.setRange(1, self.dataview.number_of_slices)
-        self.slider.setValue(1)
-        self.slider.setTracking(True)
-        
-    def show_ev(self):     
-        self.dataview.display_ev = True
-        self.change_display()
-        plot_tools.plot_image(self.img,
-                              self.img_axes,
-                              self.data,
-                              self.dataview)
-       
-    def show_wavelength(self):      
-        self.dataview.display_ev = False
-        self.change_display()
-        plot_tools.plot_image(self.img,
-                              self.img_axes,
-                              self.data,
-                              self.dataview)
+        self.wraith_window.show()                                                            
 
-    def update_graph(self):
-        self.marker.set_xdata(self.dataview.xcoordinate)
-        self.marker.set_ydata(self.dataview.ycoordinate)        
-        plot_tools.plot_graph(self.img2,
-                              self.graph_axes, 
-                              self.data,
-                              self.dataview)
-
-        
-    def update_image_from_slider(self, sliderval):
-        
-        if self.dataview.display_ev:
-            self.dataview.slider_val = sliderval-1
-        else:
-            self.dataview.slider_val = self.dataview.number_of_slices - sliderval           
-        plot_tools.plot_image(self.img,
-                              self.img_axes,
-                              self.data,
-                              self.dataview)
-                              
     def update_visualization_settings(self):
         """
         updates data view to have correct values from text boxes.
